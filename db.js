4b753aaf0e8c0601c16b182d269435bd131603cd
satanrout <satyaranjanrout81@gmail.com>
2021-03-07 16:20:41 +0530

    undoing test

--- db.js
+++ db.js
@@ -10,14 +10,11 @@ const devConfig = {
 };
 
 const proConfig = {
-  // connectionString: process.env.DATABASE_URL, //HEROKU POSTGRES ADDON
-  user: "qzgikcejbjvuyb",
-  password: "b62d240146e1f451cb46c28ecee25c837c5bbcec978e2f30572ee6d5129c9e6b",
-  host: "ec2-54-198-252-9.compute-1.amazonaws.com",
-  port: 5432,
-  database: "d9omar9qtf8alv",
+  connectionString: process.env.DATABASE_URL, //HEROKU POSTGRES ADDON
 };
 
-const pool = new Pool(proConfig);
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 module.exports = pool;
