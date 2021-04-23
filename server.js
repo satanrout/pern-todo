f4b7d773b4cd7609cd78261ca00833eb5e081146
satanrout <satyaranjanrout81@gmail.com>
2021-04-23 07:00:27 +0530

    fixed put request

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const Todo = require("./models/todoSchema");
+const mongoose = require("mongoose");
 
 const app = express();
-
-const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+mongoose.set("useFindAndModify", false);
 
 //database connection
 connectDB();
@@ -31,11 +32,11 @@ app.get("/", async (req, res) => {
 app.post("/todos", async (req, res) => {
   try {
     const { todo } = await req.body;
-    await console.log(req.body);
     const newTodo = await new Todo({
       todo: todo,
     });
-    newTodo.save((err) => res.status(404).json(err));
+    newTodo.save();
+    res.status(200).json({ status: "success", data: "added task successfuly" });
   } catch (err) {
     res.status(404).json({ status: "failed", data: err });
   }
@@ -70,12 +71,9 @@ app.get("/todos/:id", async (req, res) => {
 
 app.put("/todos/:id", async (req, res) => {
   try {
-    const { todo } = req.body;
-    const { id } = req.params;
-    await Todo.findByIdAndUpdate(id, todo);
-    res
-      .status(200)
-      .json({ status: "success", data: "task updated succesfuly" });
+    const { editValue } = await req.body;
+    await Todo.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({ status: "success", data: "updated successfuly" });
   } catch (err) {
     res.status(404).json({ status: "failed", data: err });
   }
@@ -94,6 +92,8 @@ app.delete("/todos/:id", async (req, res) => {
     res.status(404).json({ status: "failed", data: err });
   }
 });
+
+const port = process.env.PORT || 5001;
 
 app.listen(port, (req, res) => {
   console.log(`Server running  on ${port}`);
